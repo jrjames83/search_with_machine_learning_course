c748fbb289a33cfcbfa3c659ea590c7d3fee1333
Jeff James <jeffjamesny@gmail.com>
2022-07-02 03:20:15 +0000

    jupyter, more updates to training data gen script

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -3,10 +3,13 @@ import os
 import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from collections import defaultdict
+import string
 
 def transform_name(product_name):
-    # IMPLEMENT
-    return product_name
+    ''' basic string cleaning'''  
+    return product_name.lower().strip().translate(str.maketrans('', '', string.punctuation))
+
 
 # Directory for product data
 directory = r'/workspace/datasets/product_data/products/'
@@ -20,10 +23,10 @@ general.add_argument("--label", default="id", help="id is default and needed for
 # Consuming all of the product data, even excluding music and movies,
 # takes a few minutes. We can speed that up by taking a representative
 # random sample.
-general.add_argument("--sample_rate", default=33.0, type=float, help="The rate at which to sample input (default is 1.0)")
+general.add_argument("--sample_rate", default=25.0, type=float, help="The rate at which to sample input (default is 1.0)")
 
 # IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
-general.add_argument("--min_products", default=3, type=int, help="The minimum number of products per category (default is 0).")
+general.add_argument("--min_products", default=20, type=int, help="The minimum number of products per category (default is 0).")
 
 args = parser.parse_args()
 output_file = args.output
@@ -35,12 +38,14 @@ if os.path.isdir(output_dir) == False:
 if args.input:
     directory = args.input
 # IMPLEMENT:  Track the number of items in each category and only output if above the min
+products_per_category = defaultdict(list)
 min_products = args.min_products
-sample_rate = args.sample_rate
+sample_rate = args.sample_rate / 100
 names_as_labels = False
 if args.label == 'name':
     names_as_labels = True
 
+# https://github.com/gitpod-io/gitpod/issues/758
 print("Writing results to %s" % output_file)
 with open(output_file, 'w') as output:
     for filename in os.listdir(directory):
@@ -50,7 +55,7 @@ with open(output_file, 'w') as output:
             tree = ET.parse(f)
             root = tree.getroot()
             for child in root:
-                if random.random() > sample_rate:
+                if random.random() > 1 - sample_rate:
                     continue
                 # Check to make sure category name is valid and not in music or movies
                 if (child.find('name') is not None and child.find('name').text is not None and
@@ -65,4 +70,12 @@ with open(output_file, 'w') as output:
                           cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
                       # Replace newline chars with spaces so fastText doesn't complain
                       name = child.find('name').text.replace('\n', ' ')
-                      output.write("__label__%s %s\n" % (cat, transform_name(name)))
+                      products_per_category[cat].append(name)
+
+
+    filtered_categories = {cat: names_list for cat, names_list in products_per_category.items() if len(names_list) >= min_products}
+    print(f'found {len(list(filtered_categories.keys()))} Categories with more than {min_products} examples')
+    for category, products in filtered_categories.items():
+        for p in products:
+            print(category, p)
+            output.write(f"__label__{category} {transform_name(p)} \n")
