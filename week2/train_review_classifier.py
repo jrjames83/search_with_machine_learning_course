8989c85d219cdf892b54ee3b0915b8ca42f321c1
Jeff James <jeffjamesny@gmail.com>
2022-07-03 22:46:06 +0000

    reviews training notebook, error analysis, jupyter notes

--- week2/train_review_classifier.py
+++ week2/train_review_classifier.py
@@ -1,4 +1,5 @@
 import fasttext
+import pandas as pd 
 
 # shuf /workspace/datasets/reviews/output.fasttext > /workspace/datasets/reviews/shuffled_output.fasttext
 # wc -l /workspace/datasets/reviews/shuffled_output.fasttext # 221,771
@@ -6,6 +7,15 @@ import fasttext
 # head -100000 /workspace/datasets/reviews/shuffled_output.fasttext > /workspace/datasets/reviews/shuffled_output.train
 # tail -10000 /workspace/datasets/reviews/shuffled_output.fasttext > /workspace/datasets/reviews/shuffled_output.test
 
+
+df = pd.read_csv('/workspace/datasets/reviews/shuffled_output.fasttext')
+df.columns = ['blob']
+df['review'] = df['blob'].map(lambda x: x.split(' ', 1)[0])
+df['comment'] = df['blob'].map(lambda x: x.split(' ', 1)[1])
+print('label distribution')
+print(df['review'].value_counts(normalize=True))
+print('label counts')
+print(df['review'].value_counts(normalize=False))
 
 model = fasttext.train_supervised(input="/workspace/datasets/reviews/shuffled_output.train", 
                             lr=1.0, 
@@ -23,3 +33,17 @@ print(test_result)
 # Number of labels: 5
 # Progress: 100.0% words/sec/thread:  996275 lr:  0.000000 avg.loss:  0.212272 ETA:   0h 0m 0s
 # ^[[6;1R(10000, 0.6753, 0.6753)
+
+
+# Finding Common Misclassifications
+
+validation = pd.read_csv('/workspace/datasets/reviews/shuffled_output.test')
+validation.columns = ['blob']
+validation['review'] = validation['blob'].map(lambda x: x.split(' ', 1)[0])
+validation['comment'] = df['blob'].map(lambda x: x.split(' ', 1)[1])
+print('label distribution')
+print(validation['review'].value_counts(normalize=True))
+print('label counts')
+print(validation['review'].value_counts(normalize=False))
+
+validation['']
